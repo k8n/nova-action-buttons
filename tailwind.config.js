ddf6ea17576b1ac3706aa002d5875f982b1d2d22
k8n <k2@sxw.ca>
2025-02-09 17:10:19 -0500

    tailwind.config.js fix to prevent clobbering media query-based classes
    Because action-buttons css is loaded after the Nova's, redefinition of, e.g. .flex, clobbers / overrides .md:hidden.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   mode: 'jit',
   darkMode: "class",
   content: ["./resources/**/*.{js,vue}"],
+  blocklist: [
+    'flex', 'inline-flex', 'inline', 'flex-shrink-0'
+  ],
   theme: {
     extend: {},
   },
